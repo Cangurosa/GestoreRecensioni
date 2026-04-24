8d644d04da7981631e9b87c5414bd3078db4ec85
Daniel Della Posta <Danieldellaposta43@gmail.com>
2026-04-24 12:01:43 +0200

    Setup backend SpringBoot

---
+++ backend/src/main/java/it/uniroma3/siw/recensioni/RecensioniApplication.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw.recensioni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecensioniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecensioniApplication.class, args);
+	}
+
+}
